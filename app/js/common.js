2081d6d9d7d9675e9989e2355dc34348e08affcf
Vasilii <goooneoooogle@gmail.com>
2020-02-27 12:11:03 +0200

    Cart styling and functional

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,31 @@
 $(function() {
-	
+  // Cart product counter
+    // Remove products
+    $('.item--btn.minus').on('click', function() {
+      var inpVal = $(this).siblings('.item--input').val();
+      var price = $(this).parent().siblings('.cart--item__price').find('.price--value').text();
+      var total = $(this).parent().siblings('.cart--item__total').find('.price--value');
+      var totalText = $('.cart--footer__count').text();
+      if(inpVal == 1) {
+        $(this).siblings('.item--input').val(1);
+      } else {
+        $(this).siblings('.item--input').val(--inpVal);
+      }
+      total.text(price * inpVal);
+      $('.cart--footer__count').text(Number.parseInt(totalText) - 1);
+    });
+    // Add products
+    $('.item--btn.plus').on('click', function() {
+      var inpVal = $(this).siblings('.item--input').val();
+      var price = $(this).parent().siblings('.cart--item__price').find('.price--value').text();
+      var total = $(this).parent().siblings('.cart--item__total').find('.price--value');
+      var totalText = $('.cart--footer__count').text();
+      $(this).siblings('.item--input').val(++inpVal);
+      total.text(price * inpVal);
+      $('.cart--footer__count').text(Number.parseInt(totalText) + 1);
+    });
+    // Total products
+    $('.item--input').each(function(index, item) {
+      $('.cart--footer__count').text(index+1);
+    });
 });
